cb5f20768c6c8b71e37791ece981a8b9e04e67c9
Rahul Marban <rahulmarban@gmail.com>
2024-03-02 12:00:19 +0000

    feat: skeleton loader if my loader fails

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,48 +15,50 @@ import styles from "./styles/Card.module.css";
 const Card = ({ percent, title, code, data, category }: Props) => {
   return (
     <>
-      <tr className={styles.card}>
-        <td>
-          <h4>{title}</h4>
-        </td>
-        <td>
-          <span
-            title={category}
-            className={
-              category == "Theory"
-                ? styles.subjectCode
-                : [styles.subjectCode, styles.practical].join(" ")
-            }
-          >
-            {code}
-          </span>
-        </td>
+      
+        <tr className={styles.card}>
+          <td>
+            <h4>{title}</h4>
+          </td>
+          <td>
+            <span
+              title={category}
+              className={
+                category == "Theory"
+                  ? styles.subjectCode
+                  : [styles.subjectCode, styles.practical].join(" ")
+              }
+            >
+              {code}
+            </span>
+          </td>
 
-        <td>
-          <div className={styles.attendance}>
-            <span className={[styles.present, styles.green].join(" ")}>
-              {data.present}
-            </span>
-            <span className={[styles.absent, styles.red].join(" ")}>
-              {data.absent}
-            </span>
-            <span className={styles.total}>{data.total}</span>
-          </div>
-        </td>
-        <td>
-          <h3
-            className={
-              Number(percent.split(".")[0]) === 100
-                ? styles.green
-                : Number(percent.split(".")[0]) < 75
-                ? styles.red
-                : styles.percent
-            }
-          >
-            {percent}%
-          </h3>
-        </td>
-      </tr>
+          <td>
+            <div className={styles.attendance}>
+              <span className={[styles.present, styles.green].join(" ")}>
+                {data.present}
+              </span>
+              <span className={[styles.absent, styles.red].join(" ")}>
+                {data.absent}
+              </span>
+              <span className={styles.total}>{data.total}</span>
+            </div>
+          </td>
+          <td>
+            <h3
+              className={
+                Number(percent.split(".")[0]) === 100
+                  ? styles.green
+                  : Number(percent.split(".")[0]) < 75
+                  ? styles.red
+                  : styles.percent
+              }
+            >
+              {percent}%
+            </h3>
+          </td>
+        </tr>
+      
     </>
   );
 };
--- src/components/badges/DayOrder.tsx
+++ src/components/badges/DayOrder.tsx
@@ -4,6 +4,11 @@ import { useStore } from "@nanostores/react";
 import { dayOrder } from "../../stores/DayOrder";
 
 import styles from "../styles/Badge.module.css";
+import { cleanStores } from "nanostores";
+
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function DayOrder() {
   const [day, setDay] = useState<{ day_order: string } | null>(null);
@@ -26,22 +31,40 @@ export default function DayOrder() {
         dayOrder.set(res);
         setDay(JSON.parse(res));
       });
+    window.addEventListener("beforeunload", unload);
   }, []);
+
+  const unload = () => {
+    cleanStores(dayOrder);
+  };
 
   if (day) {
     return (
       <>
-        <span
-          className={
-            day.day_order.includes("No")
-              ? [styles.badge, styles.holiday].join(" ")
-              : styles.badge
-          }
-        >
-          {day.day_order.includes("No")
-            ? "Holiday"
-            : `Day Order: ${day.day_order[0]}`}
-        </span>
+        {day.day_order ? (
+          <>
+            <span
+              className={
+                day.day_order.includes("No")
+                  ? [styles.badge, styles.holiday].join(" ")
+                  : styles.badge
+              }
+            >
+              {day.day_order.includes("No")
+                ? "Holiday"
+                : `Day Order: ${day.day_order[0]}`}
+            </span>{" "}
+          </>
+        ) : (
+          <Skeleton
+            style={{
+              width: "100px",
+              height: "30px",
+              borderRadius: "12px",
+              opacity: 0.6,
+            }}
+          />
+        )}
       </>
     );
   } else return null;
