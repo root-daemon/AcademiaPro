e11d141fd76083ba6552c03108dccc3a5f2573ba
root-daemon <srivishal.sivasubramanian@gmail.com>
2024-03-03 15:27:24 +0530

    Marks

--- src/components/MarksCard.tsx
+++ src/components/MarksCard.tsx
@@ -17,19 +17,22 @@ const MarksCard = ({ name, code, category, marks }: Props) => {
 
   return (
     <>
-      <div className={[styles.card, 'm-1', 'p-1', 'container'].join(' ')}>
+      <div className={[styles.card, 'm-3', 'p-1', 'container'].join(' ')}>
         <div className="row">
           <div className={[styles.title, 'm-1'].join(' ')}>{name}</div>
         </div>
 
-        <div className="row mt-1 p-1">
+        <div className="row">
           {marks.map((element, index) => (
-            <div key={index} className="d-flex">
+            <div
+              key={index}
+              className="d-flex justify-content-around  mt-1 p-1"
+            >
               <div className={[styles.muted_title, 'col-6'].join(' ')}>
                 {element[0].slice(0, element[0].indexOf('/'))}
               </div>
-              <div className="col-6">
-                <div className={styles.attendance}>
+              <div className="col-3">
+                <div className={[styles.attendance].join(' ')}>
                   <span className={[styles.present, styles.green].join(' ')}>
                     {element[1]}
                   </span>
