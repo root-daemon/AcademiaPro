06b5d51bbd8f6be8b7da99d23808cf158b88ba88
Rahul Marban <rahulmarban@gmail.com>
2024-03-09 10:39:29 +0000

    chorus: vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,31 +1,31 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import { VitePWA } from "vite-plugin-pwa";
 import metaTags from "astro-meta-tags";
-
 import { manifest, seoConfig } from "./utils/seoConfig";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   site: seoConfig.baseURL,
   output: 'server',
   integrations: [react(), metaTags(), sitemap(), compress()],
   vite: {
-    plugins: [
-      VitePWA({
-        registerType: "autoUpdate",
-        manifest,
-        workbox: {
-          globDirectory: "dist",
-          globPatterns: [
-            "**/*.{ts,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico,tsx,js}",
-          ],
-  
-          navigateFallback: null,
-        },
-      }),
-    ],
+    plugins: [VitePWA({
+      registerType: "autoUpdate",
+      manifest,
+      workbox: {
+        globDirectory: "dist",
+        globPatterns: ["**/*.{ts,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico,tsx,js}"],
+        navigateFallback: null
+      }
+    })]
   },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  })
 });
