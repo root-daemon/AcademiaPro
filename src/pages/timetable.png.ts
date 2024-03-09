cf2ff8a4389768cd4d723ba99b285aaeab7ea598
Rahul Marban <rahulmarban@gmail.com>
2024-03-09 10:58:35 +0000

    chorus: warn if some random tried to access lol

--- src/pages/timetable.png.ts
+++ src/pages/timetable.png.ts
@@ -2,7 +2,6 @@ import fs from "fs";
 import path from "path";
 import { ImageResponse } from "@vercel/og";
 import Timetabler from "../components/Generator/TimeTableGenerator";
-import type { APIRoute } from "astro";
 
 export async function POST({ request }: { request: Request }) {
   const body = await request.json();
@@ -22,48 +21,60 @@ export async function POST({ request }: { request: Request }) {
   });
 }
 
-export const GET = ({ url, request }: { url: URL, request: Request }) => {
+export const GET = async ({ request }: { request: Request }) => {
   return new Promise((resolve) => {
-    let key;
-    const token = getToken(request.headers.get("cookie") as string);
+    const key = getToken(request.headers.get("cookie") as string);
 
-    if (token) key = token;
+    if (!key)
+      resolve(
+        new Response(
+          JSON.stringify({
+            error: "Not signed in!",
+            message:
+              "Cannot find a user token! Most probably you didn't login. Try again after logging in.",
+            url: "https://academia-pro.vercel.app/login",
+          }),
+          {
+            status: 401,
+            statusText: "Unauthorized",
+          }
+        )
+      );
     else {
-      const urlSearchParams = new URLSearchParams(url.search);
-      const params = Object.fromEntries(urlSearchParams.entries());
-      key = params.key;
-    }
+      const Inter = fs.readFileSync(path.resolve("./fonts/Inter.ttf"));
 
-    const Inter = fs.readFileSync(path.resolve("./fonts/Inter.ttf"));
+      fetch("https://academai-s-3.azurewebsites.net//course-user", {
+        method: "POST",
+        headers: {
+          "x-access-token": decodeURIComponent(key as string),
+          Host: "academai-s-3.azurewebsites.net",
+          Origin: "https://a.srmcheck.me",
+          Referer: "https://a.srmcheck.me/",
+          "Cache-Control": "max-age=604800 must-revalidate",
+        },
+      })
+        .then((r) => r.text())
+        .then((res) => {
+          const body = JSON.parse(res);
 
-    fetch("https://academai-s-3.azurewebsites.net//course-user", {
-      method: "POST",
-      headers: {
-        "x-access-token": decodeURIComponent(key as string),
-        Host: "academai-s-3.azurewebsites.net",
-        Origin: "https://a.srmcheck.me",
-        Referer: "https://a.srmcheck.me/",
-        "Cache-Control": "max-age=604800 must-revalidate",
-      },
-    })
-      .then((r) => r.text())
-      .then((res) => {
-        const body = JSON.parse(res);
-
-        resolve(
-          new ImageResponse(JSON.parse(JSON.stringify(Timetabler({ body }))), {
-            width: 2400,
-            height: 920,
-            fonts: [
+          resolve(
+            new ImageResponse(
+              JSON.parse(JSON.stringify(Timetabler({ body }))),
               {
-                name: "Inter",
-                data: Inter.buffer,
-                style: "normal",
-              },
-            ],
-          })
-        );
-      });
+                width: 2400,
+                height: 920,
+                fonts: [
+                  {
+                    name: "Inter",
+                    data: Inter.buffer,
+                    style: "normal",
+                  },
+                ],
+              }
+            )
+          );
+        });
+    }
   });
 };
 
