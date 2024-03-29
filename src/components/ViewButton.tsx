6691fd0e7baf933ee71ed276f5dd9f5e60502d71
Rahul Marban <rahulmarban@gmail.com>
2024-03-29 09:35:01 +0000

    fix: fallbacks

--- src/components/ViewButton.tsx
+++ src/components/ViewButton.tsx
@@ -1,12 +1,16 @@
+import { useStore } from "@nanostores/react";
 import { useEffect, useState } from "react";
+import { dataJSON } from "../stores/DataStore";
 
 const View = () => {
   const [data, setData] = useState(null);
+  const $data = useStore(dataJSON);
 
   useEffect(() => {
     const d = localStorage.getItem("data");
     if (d) setData(JSON.parse(d));
-  }, []);
+    else setData(JSON.parse($data));
+  }, [$data]);
 
   return data ? (
     <a href={`/timetable/${data["user"]["section"]}.png`} className="download">
